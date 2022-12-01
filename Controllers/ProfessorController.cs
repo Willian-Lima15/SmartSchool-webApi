82a75fcbd4d3362315819e4b87a05e4c7a9cae07
Willian Lima <william_slayer@hotmail.com>
2022-12-01 16:39:53 -0300

    Finalizado controller professor

--- Controllers/ProfessorController.cs
+++ Controllers/ProfessorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SmartSchool_webApi.Data;
+using SmartSchool_webApi.Models;
 
 namespace SmartSchool_webApi.Controllers
 {
@@ -23,7 +24,7 @@ namespace SmartSchool_webApi.Controllers
         {
             try
             {
-              var result = await _repo.GetAllProfessoresAsync(false);
+              var result = await _repo.GetAllProfessoresAsync(true);
               return Ok(result);
             }
             catch (Exception ex)
@@ -60,6 +61,74 @@ namespace SmartSchool_webApi.Controllers
             {
                 return BadRequest($"Erro: {ex.Message}");
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> post(Professor model)
+        {
+            try
+            {
+                _repo.Add(model);
+
+                if(await _repo.SaveChangesAsync()){
+                    return Ok(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{ProfessorId}")]
+
+        public async Task<IActionResult> put(int ProfessorId, Professor model)
+        {
+            try
+            {
+                var professor = await _repo.GetProfessorAsyncById(ProfessorId, false);
+                if(professor == null ) return NotFound();
+
+                _repo.Update(model);
+
+                if(await _repo.SaveChangesAsync())
+                {
+                    return Ok(model);
+                }
+
+            }
+             catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{ProfessorId}")]
+
+        public async Task<IActionResult> delete(int ProfessorId)
+        {
+            try
+            {
+                var professor =  _repo.GetProfessorAsyncById(ProfessorId, false);
+                if(professor == null) return NotFound();
+
+                _repo.Delete(professor);
+
+                if(await _repo.SaveChangesAsync())
+                {
+                    return Ok("Deletado");
+                }
+            }
+             catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
         }
     }
 }
